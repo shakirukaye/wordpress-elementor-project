d5756146d2117f9273db2f3ce2a4a46c309d5194
shakir <ukaye.shakir@gmail.com>
2025-09-16 22:14:28 +0530

    wordpress initial file

---
+++ public/wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor-app-bar.strings.js
@@ -0,0 +1,34 @@
+__( 'Exit to WordPress', 'elementor' );
+__( 'Help', 'elementor' );
+__( 'More', 'elementor' );
+__( 'Elementor Logo', 'elementor' );
+__( 'Integrations', 'elementor' );
+__( 'Theme Builder', 'elementor' );
+__( 'User Preferences', 'elementor' );
+__( 'Site Settings', 'elementor' );
+__( 'Structure', 'elementor' );
+__( 'Switch Device', 'elementor' );
+// translators: %s: Breakpoint label, %d: Breakpoint size.
+__( '%s (%dpx and up)', 'elementor' );
+// translators: %s: Breakpoint label, %d: Breakpoint size.
+__( '%s (up to %dpx)', 'elementor' );
+__( 'Save Changes', 'elementor' );
+__( 'History', 'elementor' );
+__( 'Keyboard Shortcuts', 'elementor' );
+__( 'Finder', 'elementor' );
+__( 'Elements', 'elementor' );
+__( 'Widgets', 'elementor' );
+__( 'Add Element', 'elementor' );
+__( 'View Page', 'elementor' );
+__( 'Save as Template', 'elementor' );
+__( 'Save Draft', 'elementor' );
+__( 'Copy and Share', 'elementor' );
+__( 'Preview Changes', 'elementor' );
+__( 'Save Options', 'elementor' );
+__( 'Save Options', 'elementor' );
+__( 'Publish', 'elementor' );
+__( 'Submit', 'elementor' );
+__( 'My Elementor', 'elementor' );
+__( 'Connect my account', 'elementor' );
+/* translators: %s: Post type label. */
+__( '%s Settings', 'elementor' );
